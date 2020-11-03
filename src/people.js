351ea05a9895c98bca271172ff996616cd59b82c
David Trussler <mail@davidtrussler.net>
2020-11-03 19:36:11 +0000

    Adds sort function - sorts on age decending

--- src/people.js
+++ src/people.js
@@ -4,3 +4,8 @@ var people = [
 	{ name: "Greg", age: 23 }, 
 	{ name: "Jean", age: 50 }
 ];
+
+// Order the people by age in decending order
+people.sort(function(a, b) {
+	return b.age - a.age; 
+}); 
